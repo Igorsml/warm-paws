dab501b9ced41abb9700f706ab3b8e0db7c0ce51
Igorsml <igorsmlff@gmail.com>
2023-07-31 23:05:40 +0300

    feat: header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Route, Routes, Router } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Page404 from "./pages/404/Page404";
 import About from "./pages/about/About";
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,23 +5,25 @@ import scss from "./Header.module.scss";
 const Header = () => {
   return (
     <header className={scss.header}>
-      <img className={scss.logo} src={logo} alt="logo" />
-      <nav>
-        <ul className={scss.navBar}>
-          <li>
-            <NavLink to="/about">О нас</NavLink>
-          </li>
-          <li>
-            <NavLink to="/slave-master">Стать хозяином</NavLink>
-          </li>
-          <li>
-            <NavLink to="/contacts">Контакты</NavLink>
-          </li>
-          <li>
-            <NavLink to="/events">События</NavLink>
-          </li>
-        </ul>
-      </nav>
+      <div className={scss.headerWrapper}>
+        <img className={scss.logo} src={logo} alt="logo" />
+        <nav className={scss.nav}>
+          <ul className={scss.navBar}>
+            <li>
+              <NavLink to="/about">О нас</NavLink>
+            </li>
+            <li>
+              <NavLink to="/slave-master">Стать хозяином</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contacts">Контакты</NavLink>
+            </li>
+            <li>
+              <NavLink to="/events">События</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
