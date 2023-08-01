318c21ff01e20c5901c48ee0e9ae624ae7da3830
Igorsml <igorsmlff@gmail.com>
2023-08-01 23:58:41 +0300

    feat: footer social icons

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,26 +1,41 @@
 import scss from "./Footer.module.scss";
 import { NavLink } from "react-router-dom";
+import Telegram from "../../assets/icons/telegram.svg";
+import Vk from "../../assets/icons/vk.svg";
+import Instagram from "../../assets/icons/instagram.svg";
+import Avito from "../../assets/icons/avito.svg";
 
 export const Footer = () => {
   return (
     <footer className={scss.footer}>
-      <p>© 2023</p>
-      <NavLink to="https://github.com/Igorsml/warm-paws" target="_blank">
-        Source code on github
-      </NavLink>
-      <div className={scss.socialIcons}></div>
-      <NavLink to="https://vk.com/public211358147">
-        <img src="./assets/icons/vk.svg" alt="vk" />
-      </NavLink>
-      <NavLink to="https://t.me/tioplyelapki">
-        <img src="../../assets/icons/telegram.svg" alt="telegram" />
-      </NavLink>
-      <NavLink to="https://instagram.com/tioplyelapki">
-        <img src="../../assets/icons/instagram.svg" alt="instagram" />
-      </NavLink>
-      <NavLink to="https://www.avito.ru/user/1bbf74c5e66eaf0216620c1c677d6961/profile?id=2430999295">
-        <img src="../../assets/icons/avito.svg" alt="avito" />
-      </NavLink>
+      <div className={scss.footerBody}>
+        <div className={scss.socialIcons}>
+          <NavLink
+            className={scss.socialIconLink}
+            to="https://vk.com/public211358147"
+          >
+            <img src={Vk} alt="vk" />
+          </NavLink>
+          <NavLink
+            className={scss.socialIconLink}
+            to="https://t.me/tioplyelapki"
+          >
+            <img src={Telegram} alt="telegram" />
+          </NavLink>
+          <NavLink
+            className={scss.socialIconLink}
+            to="https://instagram.com/tioplyelapki"
+          >
+            <img src={Instagram} alt="instagram" />
+          </NavLink>
+          <NavLink
+            className={scss.socialIconLink}
+            to="https://www.avito.ru/user/1bbf74c5e66eaf0216620c1c677d6961/profile?id=2430999295"
+          >
+            <img src={Avito} alt="avito" />
+          </NavLink>
+        </div>
+      </div>
     </footer>
   );
 };
