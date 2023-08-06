6dfee18a53e30a505a551688d708a2eb40dca719
Igorsml <igorsmlff@gmail.com>
2023-08-06 16:57:54 +0300

    fix: deploy3

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>
 );
